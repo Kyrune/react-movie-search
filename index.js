15ccdc7a8da28bcddb773194a6301fe34db1d2f2
Kyrune <jn-01@centurytel.net>
2020-06-03 10:53:14 -0500

    Add title text & className

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Main extends React.Component {
   render() {
     return (
       <div className='container'>
-        <h1>Hello world!</h1>
+        <h1 className='title'>React Movie Search</h1>
       </div>
     );
   }
