80c9c1e6aae0c2702060449de23ac59f2319fd39
Kyrune <jn-01@centurytel.net>
2020-06-12 09:07:47 -0500

    Add a div that maps thru movies

--- searchMovie.js
+++ searchMovie.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function SearchMovie() {
-
   // states - input query, movies
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
-  const [ movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
@@ -19,26 +18,37 @@ export default function SearchMovie() {
     } catch (err) {
       console.error(err);
     }
-
   };
 
-
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Title
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="i.e. Jurassic Park"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Title
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list" key={movie.id}>
+        {movies.map((movie) => (
+          <div className="card">
+            <img
+              className="card--image"
+              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+              alt={movie.title + " poster"}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
-};
+}
