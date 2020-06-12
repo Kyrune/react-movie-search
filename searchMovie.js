0d7b58e85ad09078971523d5ca1207c89dd1fd52
Kyrune <jn-01@centurytel.net>
2020-06-12 09:13:42 -0500

    Add movie descriptions

--- searchMovie.js
+++ searchMovie.js
@@ -39,15 +39,25 @@ export default function SearchMovie() {
         </button>
       </form>
       <div className="card-list" key={movie.id}>
-        {movies.filter(movie => movie.poster_path).map((movie) => (
-          <div className="card">
-            <img
-              className="card--image"
-              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-              alt={movie.title + " poster"}
-            />
-          </div>
-        ))}
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card">
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + " poster"}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>RELEASE DATE: {movie.release_date}</small>
+                  <small>RATING: {movie.vote_average}</small>
+                  <p className="card--desc">{movie.overview}</p>
+                </p>
+              </div>
+            </div>
+          ))}
       </div>
     </>
   );
